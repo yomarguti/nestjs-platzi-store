2629a38ec3192f3abc7985cbb5a38e99a2289f05
Yomar Gutierrez <yomar.guti@gmail.com>
2021-12-13 15:17:59 -0500

    Logging users

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from '../users/users.module';
+import { AuthService } from './services/auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { AuthController } from './controllers/auth.controller';
 
-@Module({})
+@Module({
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
+})
 export class AuthModule {}
